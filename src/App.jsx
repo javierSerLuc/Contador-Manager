e3c20775f090210ea67d132d72956596c05271c6
Javier Serrano <javier.serluc@gmail.com>
2022-12-31 14:24:25 +0100

    First commit
    -estructura del proyecto
    -gradientes css

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Titulo } from './components/Titulo'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="app-header">
+        <Titulo />
+      </header>
+      <main>
+        <section className='tools'>
+
+        </section>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Titulo = () => {
+  return (
+    <>
+        <h1 className='gradient-h1'>CONTADOR MANAGER</h1>
+    </>
+  )
+}
